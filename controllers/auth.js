02e17a7e820b1e04c02a018d83fa1fa01ae3eb12
YUBYEONGGUK <godkor200@gmail.com>
2021-04-06 17:00:06 +0900

    fix bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,28 +20,28 @@ const signUp = async (req, res, next) => {
     res.status(200).json({ success: true });
   } catch (err) {
     next(err);
-    console.error(err);
   }
 };
 
 const signIn = async (req, res, next) => {
   const { u_email, u_pwd } = req.body;
-  const errMsg = { msg: '로그인 정보가 정확하지 않습니다.' };
-  let emailStatus = await models.tbl_user.findOne({
-    where: { u_email: u_email },
-  });
-  let checkPassword = await bcrypt.compare(u_pwd, emailStatus.u_pwd);
-  let token = createToken(emailStatus);
   try {
-    if (!u_email || !u_pwd || !emailStatus || !checkPassword) res.send(errMsg);
-    await models.tbl_user.update(
-      { last_login_dt: new Date().toISOString() },
-      { where: { u_email: u_email } }
-    );
-    res.status(201).json({ success: true, token: token });
+    let emailStatus = await models.tbl_user.findOne({
+      where: { u_email: u_email },
+    });
+    let checkPassword = await bcrypt.compare(u_pwd, emailStatus.u_pwd);
+    let token = createToken(emailStatus);
+    if (!u_email || !u_pwd || !emailStatus || !checkPassword) {
+      res.status(403).json({ success: true, msg: 'Forbidden' });
+    } else {
+      await models.tbl_user.update(
+        { last_login_dt: new Date().toISOString() },
+        { where: { u_email: u_email } }
+      );
+      res.status(201).json({ success: true, token: token });
+    }
   } catch (err) {
     next(err);
-    console.error(err);
   }
 };
 
